7db9cc79b9a66ae889e5d9ff824c51fd7460ab77
DawidJanotaM3 <161751474+dejwkuc@users.noreply.github.com>
2024-04-12 21:14:29 +0200

    Dodaj pliki projektów.

---
+++ ToDoList/Services/ToDoListService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using ToDoList.DataModel;
+
+namespace ToDoList.Services
+{
+    public class ToDoListService
+    {
+        public IEnumerable<ToDoItem> GetItems() => new[]
+        {
+            new ToDoItem { Description = "Orzechy" },
+            new ToDoItem { Description = "Masło" },
+            new ToDoItem { Description = "Kapusta" },
+            new ToDoItem { Description = "Wieprzowina" },
+            new ToDoItem { Description = "Kotlety" },
+            new ToDoItem { Description = "Tarta" },
+            new ToDoItem { Description = "Ziemniaki 5kg" },
+        };
+    }
+}
